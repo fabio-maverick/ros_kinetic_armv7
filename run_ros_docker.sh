d291db64cdbeb618b7a8d68bc664da81c93b2108
fabio-maverick <fabio.magalhaes@gmail.com>
2018-06-11 04:54:21 -0300

    updates

---
+++ run_ros_docker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
+# Full license terms provided in LICENSE.md file.
+
+NCSDK_NAME=$1
+  if [[ -z "${ROS_NAME}" ]]; then
+    ROS_NAME=ROS_KINETIC
+fi
+
+HOST_DATA_DIR=$2
+if [[ -z "${HOST_DATA_DIR}" ]]; then
+    HOST_DATA_DIR=/data/
+fi
+
+CONTAINER_DATA_DIR=$3
+if [[ -z "${CONTAINER_DATA_DIR}" ]]; then
+    CONTAINER_DATA_DIR=/data/
+fi
+
+echo "Container name    : ${ROS_NAME}"
+echo "Host data dir     : ${HOST_DATA_DIR}"
+echo "Container data dir: ${CONTAINER_DATA_DIR}"
+NCSDK_ID=`docker ps -aqf "name=^/${ROS_NAME}$"`
+if [ -z "${ROS_ID}" ]; then
+    echo "Creating new redtail container."
+    xhost +
+    nvidia-docker run -it --privileged --network=host -v /dev:/dev -v ${HOST_DATA_DIR}:${CONTAINER_DATA_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix${DISPLAY} -p 14556:14556/udp --name=${ROS_NAME} px4-ros:kinetic-v1 bash
+else
+    echo "Found redtail container: ${NCSDK_ID}."
+    # Check if the container is already running and start if necessary.
+    if [ -z `docker ps -qf "name=^/${NCSDK_NAME}$"` ]; then
+        xhost +local:${NCSDK_ID}
+        echo "Starting and attaching to ${NCSDK_NAME} container..."
+        docker start ${NCSDK_ID}
+        docker attach ${NCSDK_ID}
+    else
+        echo "Found running ${NCSDK_NAME} container, attaching bash..."
+        docker exec -it ${NCSDK_ID} bash
+    fi
+fi
